2458edc61ea5f3071f98cd80f118d75592f89658
Jeroen Engels <jfm.engels@gmail.com>
2016-07-08 01:04:53 +0200

    Generate recommended config automatically

--- index.js
+++ index.js
@@ -4,26 +4,23 @@ const reqAll = require('req-all');
 
 const rules = reqAll('rules', {camelize: false});
 
+const externalRecommendedRules = [
+  ['no-var', 'error']
+];
+
+const recommendedRules = Object.keys(rules)
+  .map(key => [[`fp/${key}`], rules[key].meta.docs.recommended])
+  .concat(externalRecommendedRules)
+  .reduce((res, item) => {
+    res[item[0]] = item[1];
+    return res;
+  }, {});
+
 module.exports = {
   rules,
   configs: {
     recommended: {
-      rules: {
-        'no-var': 'error',
-        'fp/no-class': 'error',
-        'fp/no-delete': 'error',
-        'fp/no-events': 'error',
-        'fp/no-get-set': 'error',
-        'fp/no-let': 'error',
-        'fp/no-loops': 'error',
-        'fp/no-mutating-assign': 'error',
-        'fp/no-mutation': 'error',
-        'fp/no-nil': 'error',
-        'fp/no-proxy': 'error',
-        'fp/no-this': 'error',
-        'fp/no-throw': 'error',
-        'fp/no-unused-expression': 'error'
-      }
+      rules: recommendedRules
     }
   }
 };
